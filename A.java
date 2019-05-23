20f67490ba05281b032cfc02064eb5a674699181
Ataykano <atai.karagulov@iaau.edu.kg>
2019-05-24 02:17:23 +0600

    Create A.java
    code structure on proccess

---
+++ A.java
@@ -0,0 +1,13 @@
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+class A {
+
+
+    private String fileName;
+
+    A() {
+
+	fileName = "";
+    }
+    public String getFileName() {
